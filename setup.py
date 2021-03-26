47f0630c9bc0442ec66c7d5d0f61de31c72aff47
RyanSaxe <ryancsaxe+1@gmail.com>
2021-03-26 13:55:17 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+import re
+
+from pkg_resources import parse_requirements
+from setuptools import find_packages, setup
+
+README_FILE = 'README.md'
+REQUIREMENTS_FILE = 'requirements.txt'
+VERSION_FILE = 'mtg/_version.py'
+VERSION_REGEXP = r'^__version__ = \'(\d+\.\d+\.\d+)\''
+
+r = re.search(VERSION_REGEXP, open(VERSION_FILE).read(), re.M)
+if r is None:
+    raise RuntimeError(f'Unable to find version string in {VERSION_FILE}.')
+
+version = r.group(1)
+long_description = open(README_FILE, encoding='utf-8').read()
+install_requires = [str(r) for r in parse_requirements(open(REQUIREMENTS_FILE, 'rt'))]
+
+setup(
+    name='mtg',
+    version=version,
+    description='mtg is a collection of data science and ml projects for Magic:the Gathering',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Ryan Saxe',
+    author_email='ryancsaxe@gmail.com',
+    url='https://github.com/RyanSaxe/mtg',
+    packages=find_packages(),
+    install_requires=install_requires,
+)
