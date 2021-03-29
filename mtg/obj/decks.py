07400893e17df0e45da1b1857985873d86f72963
RyanSaxe <ryancsaxe+1@gmail.com>
2021-03-28 22:01:24 -0400

    update

--- mtg/obj/decks.py
+++ mtg/obj/decks.py
@@ -7,7 +7,7 @@ class Decks:
         self.id_to_name = {i:card_name for i,card_name in enumerate(card_names)}
         self.name_to_id = {name:idx for idx,name in self.id_to_name.items()}
         self.cards = cards.to_pandas(mapping=self.name_to_id)
-        #step 1: figure out how to reduce duplicates 
+        #step 1: figure out how to reduce duplicates
         #step 2: store deck metadata
         self.decks = df[deck_cols].to_numpy()
 
