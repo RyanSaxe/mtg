1c4c8d09e6b76ea9710ed39fab2d30054d4cee7f
RyanSaxe <ryancsaxe+1@gmail.com>
2021-03-31 23:06:51 -0400

    changed to be flexible endpoint

--- mtg/preprocess/seventeenlands.py
+++ mtg/preprocess/seventeenlands.py
@@ -23,9 +23,14 @@ def clean_bo1_games(df, cards, rename_cols=dict(), drop_cols=set()):
     df.columns = [x.lower() for x in df.columns]
     return df
 
-def get_card_rating_data(full_set, endpoint=None, join=False):
+def get_card_rating_data(expansion, endpoint=None, join=False, start=None, end=None):
     if endpoint is None:
-        endpoint = "https://www.17lands.com/card_ratings/data?expansion=KHM&format=PremierDraft&start_date=2021-01-01&end_date=2021-02-19"
+        endpoint = f'https://www.17lands.com/card_ratings/data?expansion={expansion.upper()}&format=PremierDraft'
+        if start is not None:
+            endpoint += f'&start_date={start}'
+        if end is not None:
+            endpoint += f'&end_date={end}'
+    full_set = CardSet(["set=" + expansion])
     card_json = requests.get(endpoint).json()
     card_df = pd.DataFrame(card_json)
     flip_cards = {x.name.split("//")[0].strip():x.name.split("//")[1].strip() for x in full_set.cards if "//" in x.name}
@@ -40,8 +45,8 @@ def get_card_rating_data(full_set, endpoint=None, join=False):
     scry = full_set.to_dataframe()
     if join:
         card_df = card_df.join(scry, how="left", rsuffix="__extra")
-        extras = [col for col in card_df.columns if col.endsith("__extra")]
-        card_df = card_df.drop(extras)
+        extras = [col for col in card_df.columns if col.endswith("__extra")]
+        card_df = card_df.drop(extras, axis=1)
     return card_df
 
 def add_archetypes(df, cardset, min_2c_basics=5, min_1c_basic=11):
