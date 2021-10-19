7e94dd7ec261dcd2cf94b9cd42001249e23b6551
RyanSaxe <ryancsaxe+1@gmail.com>
2021-10-19 18:49:28 -0400

    validation verbose implementation

--- mtg/ml/trainer.py
+++ mtg/ml/trainer.py
@@ -51,6 +51,7 @@ class Trainer:
                 )
             extras = {k:[] for k in print_keys}
             losses = []
+            val_losses = []
             for i in range(n_batches):
                 batch_idx = self.batch_ids[i * batch_size:(i+1) * batch_size]
                 batch_features = self.features[batch_idx,:]
@@ -61,12 +62,20 @@ class Trainer:
                 else:
                     batch_weights = None
                 loss = self._step(batch_features, batch_target, batch_weights)
+                #run model as if not training on validation data to get out of sample performance
+                if self.val_features is not None:
+                    val_out = self.model(self.val_features, training=None)
+                    val_loss = self.model.loss(self.val_target, val_out, sample_weight=self.val_weights)
+                    val_losses.append(np.average(val_loss))
                 losses.append(np.average(loss))
                 for attr_name in extras.keys():
                     attr = getattr(self.model, attr_name, None)
                     extras[attr_name].append(attr)
                 if verbose:
-                    progress.set_postfix(loss=np.average(losses), **{k:np.average(v) for k,v in extras.items()})
+                    if self.val_features is not None:
+                        progress.set_postfix(loss=np.average(losses), val_loss=np.average(val_losses), **{k:np.average(v) for k,v in extras.items()})
+                    else:
+                        progress.set_postfix(loss=np.average(losses), **{k:np.average(v) for k,v in extras.items()})
                     progress.update(1)
             if verbose:
                 progress.close()
