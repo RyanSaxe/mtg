f28bd1c3872b58c353b2e3b455a4f9cd48e3a227
RyanSaxe <ryancsaxe+1@gmail.com>
2021-12-01 14:30:19 -0500

    getting rid of extra axis

--- mtg/ml/generator.py
+++ mtg/ml/generator.py
@@ -149,7 +149,12 @@ class DraftGenerator(MTGDataGenerator):
             weights = self.weights[indices]/self.weights[indices].sum()
         else:
             weights = None
-        return (draft_info.astype(np.float32), positions.astype(np.int32)), picks.astype(np.int32), weights
+        return (
+            draft_info.astype(np.float32),
+            np.squeeze(positions.astype(np.int32)),
+            np.squeeze(picks.astype(np.int32)),
+            weights,
+        )
 
 class DeckGenerator(MTGDataGenerator):
     def __init__(
