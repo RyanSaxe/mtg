fixed memory issue because I forgot to delete a debugging gradient monitor

mtg/ml/generator.py
@@ -153,9 +153,9 @@ class DraftGenerator(MTGDataGenerator):
             weights = self.weights[indices]/self.weights[indices].sum()
         else:
             weights = None
-        draft_info = tf.convert_to_tensor(draft_info.astype(np.float32), dtype=tf.float32)
-        positions = tf.convert_to_tensor(positions.astype(np.int32), dtype=tf.int32)
-        picks = tf.convert_to_tensor(picks.astype(np.float32), dtype=tf.float32)
+        draft_info = draft_info.astype(np.float32)
+        positions = positions.astype(np.int32)
+        picks = picks.astype(np.float32)
         return (draft_info, positions), picks, weights
 
 class DeckGenerator(MTGDataGenerator):

mtg/ml/trainer.py
@@ -46,7 +46,6 @@ class Trainer:
             assert self.val_features is None
             assert self.val_target is None
             assert self.val_weights is None
-        self.grads = []
     
     def _step(self, batch_features, batch_target, batch_weights):
         with tf.GradientTape() as tape:
@@ -57,7 +56,6 @@ class Trainer:
         grads = tape.gradient(loss, self.model.trainable_variables)
         if self.clip:
             grads, _ = tf.clip_by_global_norm(grads, self.clip)
-        self.grads.append(grads)
         self.model.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
         return loss, metrics
     def train(self, n_epochs, batch_size=32, verbose=True, print_keys=[]):
@@ -132,5 +130,5 @@ class Trainer:
                 progress.close()
             if self.generator is not None:
                 self.generator.on_epoch_end()
-                if self.val_generator is not None:
-                    self.val_generator.on_epoch_end()
+            if self.val_generator is not None:
+                self.val_generator.on_epoch_end()