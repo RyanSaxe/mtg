9319ed3d6f0dfa89052806d8160da07a96f3851c
RyanSaxe <ryancsaxe+1@gmail.com>
2021-12-05 23:49:56 -0500

    finally have things set up properly to store

--- mtg/ml/trainer.py
+++ mtg/ml/trainer.py
@@ -104,7 +104,7 @@ class Trainer:
                 if self.val_generator is not None:
                     val_features, val_target, val_weights = self.val_generator[i]
                     # must get attention here to serialize the input for saving
-                    val_output, attention = self.model(val_features, training=False)
+                    val_output = self.model(val_features, training=False)
                     val_loss = self.model.loss(val_target, val_output, sample_weight=val_weights)
                     val_metrics = self.model.compute_metrics(val_target, val_output, sample_weight=val_weights)
                     extra_metrics['val_top1'].append(val_metrics[0])
