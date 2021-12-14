ca986a694617c5db42b00aa2c7f7475c187d9d5b
RyanSaxe <ryancsaxe+1@gmail.com>
2021-12-14 13:52:14 -0500

    used wrong object

--- mtg/utils/display.py
+++ mtg/utils/display.py
@@ -161,11 +161,11 @@ def draft_log_ai(draft_log_url, model, t=None, n_cards=None, idx_to_name=None, r
     r = requests.post(url = "https://www.17lands.com/api/submit_draft", json = js)
     r_js = r.json()
     try:
-        draft_id = r['id']
+        draft_id = r_js['id']
         return f"https://www.17lands.com/submitted_draft/{draft_id}"
     except:
         warnings.warn("Draft Log Upload Failed. Returning sent JSON to help debug.")
-        return js
+        return (js, r)
 
 def display_draft(df, cmap=None, pack=None):
     if pack is not None:
