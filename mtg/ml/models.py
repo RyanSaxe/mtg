5faf356fcf4e8b682f8452db5fe4bb5a0cd2d1f5
RyanSaxe <ryancsaxe+1@gmail.com>
2021-12-23 22:27:38 -0500

    need 2nd axis to be there for metrics

--- mtg/ml/models.py
+++ mtg/ml/models.py
@@ -563,24 +563,24 @@ class DeckBuilder(tf.Module):
     #we only wrap in tf.function for this to be serialized
     @tf.function
     def build_decks(self,pools):
-        if len(pools.shape) == 1:
-            pools = tf.expand_dims(pools, axis=0)
+        if len(pools.shape) == 2:
+            pools = tf.expand_dims(pools, axis=1)
         deck = tf.zeros_like(pools, dtype=np.float32)
-        basics = tf.zeros((pools.shape[0], 5), dtype=np.float32)
+        basics = tf.zeros((pools.shape[0], 1, 5), dtype=np.float32)
         for i in range(0,40):
             cards_to_add = self.__call__((pools, deck, basics), training=False)
             if i < 20:
                 cards_to_add = cards_to_add[:,5:]
                 card_to_add = tf.math.argmax(cards_to_add)
-                deck[:,card_to_add] += 1
-                pools[:,card_to_add] -= 1
+                deck[:,:,card_to_add] += 1
+                pools[:,:,card_to_add] -= 1
             else:
                 card_to_add = tf.math.argmax(cards_to_add)
                 if card_to_add < 5:
-                    basics[:,card_to_add] += 1
+                    basics[:,:,card_to_add] += 1
                 else:
-                    deck[:,card_to_add - 5] += 1
-                    pools[:,card_to_add - 5] -= 1
+                    deck[:,:,card_to_add - 5] += 1
+                    pools[:,:,card_to_add - 5] -= 1
         return basics, deck
 
     def save(self, cards, location):
